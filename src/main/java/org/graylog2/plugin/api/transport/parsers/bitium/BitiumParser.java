8ada1fd8a1fd8da33a9f14478fdfa02bbc227b3b
Romulo Salazar <rsalazar@opentable.com>
2018-03-15 12:52:22 -0700

    parser modifications

--- src/main/java/org/graylog2/plugin/api/transport/parsers/bitium/BitiumParser.java
+++ src/main/java/org/graylog2/plugin/api/transport/parsers/bitium/BitiumParser.java
@@ -37,13 +37,8 @@ public class BitiumParser extends Parser{
                 if (jsonArray.length() > 0) {
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonobject = jsonArray.getJSONObject(i);
-
                         //Copy base into eventData
                         Map<String, Object> eventData = Maps.newHashMap(this.baseEventData);
-                        eventData.put("_type", jsonobject.get("type"));
-                        eventData.put("_name", jsonobject.getJSONObject("actor").getString("name"));
-                        eventData.put("_email", jsonobject.getJSONObject("actor").getString("email"));
-                        eventData.put("_created_at", jsonobject.getString("created_at"));
                         eventData.put("short_message", jsonobject.toString());
                         publishToGraylog(eventData, messageInput);
                     }
